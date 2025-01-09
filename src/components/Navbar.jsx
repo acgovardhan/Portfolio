7713ff11f5f76e83f7ff89133358b650d76efe21
acgovardhan <mail2acgovardhan@gmail.com>
2025-01-09 18:39:16 +0530

    fiirst commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+import { FaLinkedin,FaInstagram,FaGithub } from 'react-icons/fa'
+const Navbar = () => {
+  return (
+    <>
+    <nav className="mt-1  mb-20 flex items-center justify-between py-6 lg:mt-5 lg:px-20"><div>
+      <img className="mx-2 w-20"src={logo} />
+    </div>
+    <div className='mx-4 text-white flex items-center justify-center py-4 gap-4 text-4xl'>
+      <a href='https://github.com/acgovardhan'target='_blank' rel="noopener noreferrer"><FaLinkedin/></a>
+      <a href='https://github.com/acgovardhan'target='_blank' rel="noopener noreferrer"><FaInstagram/></a>
+      <a href='https://github.com/acgovardhan'target='_blank' rel="noopener noreferrer"><FaGithub/></a>
+      
+      
+    </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
