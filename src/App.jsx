923415220c3aa70ef013d916fa13a94b84497236
acgovardhan <mail2acgovardhan@gmail.com>
2025-01-20 18:57:23 +0530

    nndn

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
-import Technologies from "./components/technologies";
+import Technologies from "./components/Technologies";
 
 
 export default function App() {
