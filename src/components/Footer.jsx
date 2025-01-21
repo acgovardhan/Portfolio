df82ce31e7465347900e5bc33c002a83086785b2
acgovardhan <mail2acgovardhan@gmail.com>
2025-01-22 00:26:54 +0530

    overflow-hidden

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import { motion } from 'framer-motion';
 const Footer = () => {
   return (
-    <div className="mt-10 pb-10 ">
+    <div className="mt-10 pb-10 relative overflow-hidden">
       <div class="border-t border-white opacity-25"></div>
       <div className="text-sm text-center tracking-tighter text-white mt-5">
         <motion.div whileInView={{ opacity: 1, x: 0 }}
         initial={{ opacity: 0, x: -100 }}
-        transition={{ duration: 1 }}>+91 9645100641</motion.div>
+        transition={{ duration: 1 }} className="overflow-hidden">+91 9645100641</motion.div>
 
         <motion.div whileInView={{ opacity: 1, x: 0 }}
         initial={{ opacity: 0, x: 100 }}
-        transition={{ duration: 1 }}>Thiruvanathapuran , Kerala</motion.div>
+        transition={{ duration: 1 }} className="overflow-hidden">Thiruvanathapuran , Kerala</motion.div>
 
         <motion.div whileInView={{ opacity: 1, x: 0 }}
         initial={{ opacity: 0, x: -100 }}
-        transition={{ duration: 1 }}>mail2acgovardhan@gmail.com</motion.div>
+        transition={{ duration: 1 }} className="overflow-hidden">mail2acgovardhan@gmail.com</motion.div>
       </div>
     </div>
   );
