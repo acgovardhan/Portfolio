f0e1aca6ca90a8bfa6f55271e693312ce7d6ff33
acgovardhan <mail2acgovardhan@gmail.com>
2025-01-22 00:18:02 +0530

    moving feature

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import { FaHeart } from 'react-icons/fa';
+import { motion } from 'framer-motion';
 const Footer = () => {
   return (
     <div className="mt-10 pb-10 ">
       <div class="border-t border-white opacity-25"></div>
       <div className="text-sm text-center tracking-tighter text-white mt-5">
-        <div>+91 9645100641</div>
-        <div>Thiruvanathapuran , Kerala</div>
-        <div>mail2acgovardhan@gmail.com</div>
-        {/* <div className='mt-10 flex justify-center'>Made with . . .<FaHeart/></div> */}
+        <motion.div whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1 }}>+91 9645100641</motion.div>
+
+        <motion.div whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: 100 }}
+        transition={{ duration: 1 }}>Thiruvanathapuran , Kerala</motion.div>
+
+        <motion.div whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1 }}>mail2acgovardhan@gmail.com</motion.div>
       </div>
     </div>
   );
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -4,17 +4,37 @@ import { DiJava } from 'react-icons/di'
 import { FaC } from 'react-icons/fa6'
 import { FaReact } from 'react-icons/fa'
 import { FaJs } from 'react-icons/fa'
+import { animate, inertia, inView, motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  initial: { y: -15 },
+  animate: {
+    y: [15, -15],
+    transition: {
+      duration: duration,
+      ease: "easeInOut",
+
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 
 const Technologies = () => {
   return (
     <div>
       <div><h2 className='text-white flex justify-center text-3xl lg:text-6xl'>Technologies</h2></div>
-      <div className='text-white flex justify-evenly py-20 text-7xl'>
-        <div className='w-50 text-yellow-300'><FaJs/></div>
-        <div className='w-50 text-orange-600 bg-white rounded-2xl'><DiJava/></div>
-        <div className='w-50 text-blue-500'><FaReact/></div>
-        <div className='w-50 text-yellow-300 rounded-xl'><FaPython/></div>
-        <div className='w-50'><FaC/></div>
+      <div className='text-white flex justify-evenly py-20 text-7xl mt-10'>
+        <motion.div initial="initial"
+          animate="animate" variants={iconVariants(1.5)} className='w-50 text-yellow-300'><FaJs/></motion.div>
+        <motion.div  initial="initial"
+          animate="animate" variants={iconVariants(1)} className='w-50 text-orange-600 bg-white rounded-2xl'><DiJava/></motion.div>
+        <motion.div initial="initial"
+          animate="animate" variants={iconVariants(0.8)} className='w-50 text-blue-500'><FaReact/></motion.div>
+        <motion.div  initial="initial"
+          animate="animate" variants={iconVariants(1)} className='w-50 text-yellow-300 rounded-xl'><FaPython/></motion.div>
+        <motion.div initial="initial"
+          animate="animate"  variants={iconVariants(1.5)}className='w-50'><FaC/></motion.div>
         
       </div>
     </div>
